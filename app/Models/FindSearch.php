aa962171a73ec487563d0dd952ae5d05a36184d4
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-08-31 12:27:17 -0300

    v0.21.08.31

--- app/Models/FindSearch.php
+++ app/Models/FindSearch.php
@@ -50,14 +50,14 @@ class FindSearch extends Model
 			  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
 			</ol>
 			<div class="carousel-inner">
-			  <div class="carousel-item active">
+			  <div class="carousel-item ">
 				<img class="d-block w-100" src="'.base_url('img/banner_01.png').'" alt="First slide">
 			  </div>
-			  <div class="carousel-item">
-				<img class="d-block w-100" src="'.base_url('img/banner_01.png').'" alt="Second slide">
+			  <div class="carousel-item active">
+				<img class="d-block w-100" src="'.base_url('img/banner_02.png').'" alt="Second slide">
 			  </div>
 			  <div class="carousel-item">
-				<img class="d-block w-100" src="'.base_url('img/banner_01.png').'" alt="Third slide">
+				<img class="d-block w-100" src="'.base_url('img/banner_03.png').'" alt="Third slide">
 			  </div>
 			</div>
 			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
