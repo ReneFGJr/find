27deff83541e50e4345fa304d0eaf3ee2fa26b02
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-06-21 10:30:07 -0300

    A

---
+++ angular/find/src/app/000_core/page/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+})
+export class FindAboutComponent {
+  public logo = '/assets/img/logo/logo_find.png';
+}
