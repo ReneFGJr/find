25bc1e81f719a8eea46f1d39ad6e524168f417b7
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-11-12 11:24:17 -0300

    A

--- findAPP/src/app/020_find/widget/classification/classification-cdu/classification-cdu.component.ts
+++ findAPP/src/app/020_find/widget/classification/classification-cdu/classification-cdu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-classification-cdu',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './classification-cdu.component.scss'
 })
 export class ClassificationCDUComponent {
-
+  @Input() data: any;
 }
