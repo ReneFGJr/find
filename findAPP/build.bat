14ed0a804415b35f5e9aa2128ad21d74363591c0
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-04-17 12:18:51 -0300

    Uso da IA

--- findAPP/build.bat
+++
@@ -1,6 +0,0 @@
-rem ng build --output-path dist --base-href './'.
-rem ng build --output-path dist --base-href '/'.
-rem ng build --output-path dist
-ng build --output-path=../web --base-href=/find/web/ --deploy-url=/find/web/
-
-echo "Copiando arquivo"
