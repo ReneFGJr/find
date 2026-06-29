5acdc9733e8462909a6c0a8271818e24bbc8f08c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-06-29 16:22:28 -0300

    Update tombo.php

--- v2/app/Views/catalog/tombo.php
+++ v2/app/Views/catalog/tombo.php
@@ -25,7 +25,8 @@
                     'book' => $book,
                     'library' => $library,
                     'itemInfo' => $row,
-                    'meta' => $meta
+                    'meta' => $meta,
+                    'perfil' => [],
                 ]);
             }
             ?>
